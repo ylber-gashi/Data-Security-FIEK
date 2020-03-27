55d939f86fcea36313eb2e63c2991602b4c76528
ylber-gashi <58399076+ylber-gashi@users.noreply.github.com>
2020-03-28 00:05:50 +0100

    Code formatting

--- paketa/MorseCode.java
+++ paketa/MorseCode.java
@@ -4,13 +4,17 @@ import java.io.*;
 import java.util.*;
 import javax.sound.sampled.*;
 import java.util.List;
+
 public class MorseCode {
     private static final int DOT = 200, DASH = DOT * 3, FREQ = 800;
+    
     public static HashMap<String, String> encodeMap(){
         HashMap<String,String> morseDict = new HashMap<String, String>();
+        
         List<Character> letters = Arrays.asList(' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
                 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
                 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
+        
         List<String> morseLetters = Arrays.asList("/", ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
                 "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
                 "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-",
@@ -19,17 +23,15 @@ public class MorseCode {
             for (int i = 0; i < letters.size(); i++) {
                 morseDict.put(Character.toString(letters.get(i)), morseLetters.get(i));
             }
-
         return morseDict;
     }
 
     public static HashMap<String,String> decodeMap() {
         HashMap<String,String> morseDict = new HashMap<String, String>();
+        
         List<Character> letters = Arrays.asList(' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
                 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
                 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
-
-
 
         List<String> morseLetters = Arrays.asList("/", ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
                 "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
@@ -39,13 +41,13 @@ public class MorseCode {
             for (int i = 0; i < letters.size(); i++) {
                 morseDict.put(morseLetters.get(i), Character.toString(letters.get(i)));
             }
-
         return morseDict;
     }
 
     public static String encode(String x) {
         x = x.toLowerCase();
         String encodedText="";
+        
         for (int i = 0; i < x.length(); i++) {
             encodedText+=encodeMap().get(Character.toString(x.charAt(i))) + " ";
         }
@@ -55,16 +57,20 @@ public class MorseCode {
     public static String decode(String x) {
         String[] textArray = x.split(" ");
         String decodedText="";
+        
         for (int i = 0; i < textArray.length; i++) {
             decodedText += decodeMap().get(textArray[i]);
         }
         return decodedText;
     }
-    public static void morseAudio(String text) throws IOException, LineUnavailableException, InterruptedException {
+    public static void morseAudio(String text) throws IOException, LineUnavailableException, InterruptedException 
+    {
         String audio = encode(text);
         boolean sound = !Arrays.asList(audio).contains("-n");
+        
         for (char note : audio.toCharArray()) {
             System.out.print(note == ' ' ? "" : note);
+            
             if (sound) {
                 try (SourceDataLine sdl = AudioSystem.getSourceDataLine(new AudioFormat(8000F, 8, 1, true, false))) {
                     sdl.open(sdl.getFormat());
@@ -76,9 +82,7 @@ public class MorseCode {
                 }
             }
             Thread.sleep(DOT / 5);
-        }
-    }
-
-    
+      }
+    } 
 }
 
