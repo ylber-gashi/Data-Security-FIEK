1280ab83c3d67c0e5cf00237e395c2699778113c
ylber-gashi <58399076+ylber-gashi@users.noreply.github.com>
2020-04-26 21:13:52 +0200

    Krijimi i klases ExportImport dhe deklarimi i metodave qe do te perdoren

---
+++ paketa/ExportImport.java
@@ -0,0 +1,30 @@
+package paketa;
+
+public class ExportImport {
+
+    public void exportKey() throws Exception {
+
+    }
+
+    public void importKey() throws FileNotFoundException {
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getElement() throws Exception {
+    }
+
+    public String readFile() throws FileNotFoundException {
+
+    }
+    public void writeFile() throws Exception{
+
+    }
+
+    public void deleteUser() {
+
+    }
+}
