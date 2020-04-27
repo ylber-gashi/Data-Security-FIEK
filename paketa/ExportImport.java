3047e391f8a121eecbf60005442899c46385aa42
ylber-gashi <58399076+ylber-gashi@users.noreply.github.com>
2020-04-27 22:32:28 +0200

    Shtova metoden qe e largon kopjen e qelesit pasi eksportohet

--- paketa/ExportImport.java
+++ paketa/ExportImport.java
@@ -40,6 +40,7 @@ public class ExportImport {
         PrintWriter writer = new PrintWriter(filename);
         try{
             writer.write(text);
+            System.out.println("Celesi publik u ruajt ne fajllin '" + filename + "'.");
         }catch (Exception e){
             e.getMessage();
         }finally {
@@ -48,7 +49,11 @@ public class ExportImport {
 
     }
 
-    public void deleteUser() {
-
+    public void deleteUser(String user) {
+        File file = new File(user);
+        if ((file.exists())) {
+            file.delete();
+        } else
+            System.out.println("Gabim tek fshierja: Celesi '"+ user +"' nuk ekziston.");
     }
 }
