6ed469daac10535e2b44e416b75da3f6298f4f25
ylber-gashi <58399076+ylber-gashi@users.noreply.github.com>
2020-06-05 23:49:25 +0200

    Krijimi i klases LoginStatus
    Kjo klase do të përdoret për komandat login dhe status

---
+++ LoginStatus.java
@@ -0,0 +1,26 @@
+package paketa;
+
+public class LoginStatus {
+
+    public void login(String username) throws Exception {
+
+    }
+
+    public void status(String token) throws Exception {
+       
+    }
+
+    public String generateToken(String user) throws Exception {
+
+    }
+
+    public String Signature(String user,byte[] userdate) throws Exception {
+
+    }
+
+
+    public void writeFile(String text, String filename) throws Exception{
+
+    }
+
+}
